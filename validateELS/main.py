083849504a1641e00703032dd60beafe4a4cd5dd
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2024-07-09 11:23:59 +0900

    [feat] MCS 결과 DB 삽입 기능 추가

--- validateELS/main.py
+++ validateELS/main.py
@@ -1,40 +1,50 @@
 import pymysql
 
 from config import DB_CONFIG
-from validateELS.eval_functions import eval_prod_with_one_prop, eval_prod_with_two_prop, eval_prod_with_three_prop
+from eval_functions import eval_prod_with_one_prop, eval_prod_with_two_prop, eval_prod_with_three_prop
+
+
+# 데이터 삽입 쿼리
+insert_query = '''
+INSERT INTO MCS_result_hantoo (loss_probability)
+VALUES (%s)
+WHERE
+'''
+
 def change_data_format(row):
     data = {}
-    data["interest_rate"] = float(row[3]) / 100
-    data["x_volatility"] = float(row[4])
-    if row[5] != None:
-        data["y_volatility"] = float(row[5])
-    if row[6] != None:
-        data["z_volatility"] = float(row[6])
-    if row[7] != None:
-        data["rho_xy"] = float(row[7])
-    if row[8] != None:
-        data["rho_xz"] = float(row[8])
+    data["interest_rate"] = float(row[7]) / 100
+    data["x_volatility"] = float(row[8])
     if row[9] != None:
-        data["rho_yz"] = float(row[9])
-    data["coupon_rate"] = float(row[10])
-    data["expiration_coupon_rate"] = float(row[11])
-    data["kib"] = float(row[12])
+        data["y_volatility"] = float(row[9])
+    if row[10] != None:
+        data["z_volatility"] = float(row[10])
+    if row[11] != None:
+        data["rho_xy"] = float(row[11])
+    if row[12] != None:
+        data["rho_xz"] = float(row[12])
+    if row[13] != None:
+        data["rho_yz"] = float(row[13])
+    data["coupon_rate"] = float(row[15])
+    data["expiration_coupon_rate"] = float(row[16])
+    data["kib"] = float(row[17])
 
     temp = []
-    for i in range(13, 19):
+    for i in range(18, 24):
         temp.append(float(row[i]) / 100)
     data["payment_conditions"] = temp
 
-    data["initial_price_evaluation_date"] = row[19].strftime("%Y-%m-%d")
+    data["initial_price_evaluation_date"] = row[24].strftime("%Y-%m-%d")
 
     temp = []
-    for i in range(20, 25):
+    for i in range(25, 30):
         temp.append(row[i].strftime("%Y-%m-%d"))
     data["early_repayment_evaluation_dates"] = temp
 
-    data["maturity_date"] = row[25].strftime("%Y-%m-%d")
+    data["maturity_date"] = row[30].strftime("%Y-%m-%d")
 
     return data
+
 
 # MySQL 데이터베이스 연결
 connection = pymysql.connect(**DB_CONFIG)
@@ -43,7 +53,7 @@ connection = pymysql.connect(**DB_CONFIG)
 try:
     with connection.cursor() as cursor:
         # SQL 쿼리
-        sql = "SELECT * FROM ELS_hantoo where num_equity = 1 limit 20"
+        sql = "SELECT * FROM MCS_result_hantoo"
 
         # 쿼리 실행
         cursor.execute(sql)
@@ -51,18 +61,33 @@ try:
         # 결과 가져오기
         result = cursor.fetchall()
 
+        update_query = "UPDATE MCS_result_hantoo SET loss_probability = %s WHERE id = %s"
+
+
         # 결과 출력
         for row in result:
             data = change_data_format(row)
+            id = row[0]
 
-            if row[2] == 3:
+            if row[3] != None:
+                continue
+
+            if row[1] == 3:
                 price, early_redempted_probabilities, final_gain_prob, loss_prob = eval_prod_with_three_prop(data)
-            elif row[2] == 2:
+            elif row[1] == 2:
                 price, early_redempted_probabilities, final_gain_prob, loss_prob = eval_prod_with_two_prop(data)
             else:
                 price, early_redempted_probabilities, final_gain_prob, loss_prob = eval_prod_with_one_prop(data)
 
-            print(loss_prob)
+            print(loss_prob, row[2])
+            cursor.execute(update_query, (loss_prob, id))
+            connection.commit()
 
 finally:
     connection.close()
+
+
+
+
+
+
